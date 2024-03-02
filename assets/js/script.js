4d169520f43008f3dee0058c3010d042f6d78f74
ankushh19 <131458678+ankushh19@users.noreply.github.com>
2024-03-02 15:20:05 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/* Navbar toggler */
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/* Header scroll */
+
+const $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", (e) => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/* Add to favourite button */
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-button]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
+});
